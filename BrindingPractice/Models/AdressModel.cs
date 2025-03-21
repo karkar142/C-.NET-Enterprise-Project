a31f168e45d7d00b3744c262d2e0f80343093d3c
Wai Zin Aung <waizinaung.mm142@gmail.com>
2025-03-21 12:50:12 +0800

    First commit

---
+++ BrindingPractice/Models/AdressModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrindingPractice.Models
+{
+    public class AdressModel
+    {
+        public string Country{get;set;}
+         public string City{get;set;}
+         public string Township{get;set;}
+         public string ZipCode{get;set;}
+    }
+}
---
+++ BrindingPractice/Models/EmployeeModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrindingPractice.Models
+{
+    public class EmployeeModel
+    {
+         public int id{get;set;}
+         public string Name{get;set;}
+         public string Email{get;set;}
+    }
+}
